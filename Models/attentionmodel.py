96063d60d3bdb3aa2cba7d3b335d6f64268b598e
unknown <ryancwongsa@gmail.com>
2018-11-14 00:25:08 +0000

    working version

--- Models/attentionmodel.py
+++ Models/attentionmodel.py
@@ -86,7 +86,6 @@ class AttentionModel(object):
     
             
     def evaluate(self, image):
-        print(tf.train.latest_checkpoint('training_checkpoints2'))
         with tf.contrib.eager.restore_variables_on_create(tf.train.latest_checkpoint('training_checkpoints2')):
             attention_plot = np.zeros((self.tokenisation.max_length, self.attention_features_shape))
 
@@ -117,6 +116,40 @@ class AttentionModel(object):
             attention_plot = attention_plot[:len(result), :]
             return result, attention_plot
 
+    def evaluate2(self, image):
+        with tf.contrib.eager.restore_variables_on_create(tf.train.latest_checkpoint('training_checkpoints2')):
+            attention_plot = np.zeros((self.tokenisation.max_length, self.attention_features_shape))
+
+            hidden = self.decoder.reset_state(batch_size=1)
+#             print(load_image(image)[0])
+#             temp_input = tf.expand_dims(load_image(image)[0], 0)
+            img = tf.keras.applications.inception_v3.preprocess_input(image.astype(np.float32))
+            temp_input = tf.expand_dims(img, 0)
+
+            img_tensor_val = self.image_features_extract_model(temp_input)
+            img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+
+            features = self.encoder(img_tensor_val)
+
+            dec_input = tf.expand_dims([self.tokenizer.word_index['<start>']], 0)
+            result = []
+
+            for i in range(self.tokenisation.max_length):
+                predictions, hidden, attention_weights = self.decoder(dec_input, features, hidden)
+
+                attention_plot[i] = tf.reshape(attention_weights, (-1, )).numpy()
+
+                predicted_id = tf.multinomial(predictions, num_samples=1)[0][0].numpy()
+                result.append(self.tokenisation.index_word[predicted_id])
+
+                if self.tokenisation.index_word[predicted_id] == '<end>':
+                    return result, attention_plot
+
+                dec_input = tf.expand_dims([predicted_id], 0)
+
+            attention_plot = attention_plot[:len(result), :]
+            return result, attention_plot
+        
     def save_checkpoint(self, checkpoint_dir = './training_checkpoints'):
         self.checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
         self.checkpoint = tf.train.Checkpoint(optimizer=self.optimizer,
--- app/server.py
+++ app/server.py
@@ -9,7 +9,10 @@ import codecs
 from PIL import Image
 import os 
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-sys.path.append("D:\\Repos\\ImageCaptioningWithAttention")
+matching = [s for s in sys.path if "app" in s]
+if len(matching)>0:
+    appendThis = (matching[0][:-4])
+sys.path.append(appendThis)
 
 import tensorflow as tf
 tf.enable_eager_execution()
@@ -44,13 +47,7 @@ inmodel = InceptionModel()
 tokenisations = Tokenisations(True, "configs/tokenizer.pkl", "configs/token_dict.npy")
 vis = Visualiser()
 attentionModel = AttentionModel(tokenisations, embedding_dim, units, len(tokenisations.tokenizer.word_index), BATCH_SIZE, attention_features_shape, inmodel.image_features_extract_model)
-image_url = 'https://tensorflow.org/images/surf.jpg'
-image_extension = image_url[-4:]
-image_path = tf.keras.utils.get_file('image'+image_extension, 
-                                     origin=image_url)
-print(image_path)
-result, attention_plot = attentionModel.evaluate(image_path)
-print ('Prediction Caption:', ' '.join(result))
+
 # vis.plot_attention(image_path, result, attention_plot)
 # opening the image
 # Image.open(image_path)
@@ -79,15 +76,15 @@ def index(request):
 async def analyze(request):
     data = await request.form()
     img_bytes = await (data['file'].read())
+    image_path = np.array(Image.open(BytesIO(img_bytes)).resize((299,299)))[:,:,0:3]
+#     image_url = 'https://tensorflow.org/images/surf.jpg'
+#     image_extension = image_url[-4:]
+#     image_path = tf.keras.utils.get_file('image'+image_extension, 
+#                                          origin=image_url)
     
-    image_url = 'https://tensorflow.org/images/surf.jpg'
-    image_extension = image_url[-4:]
-    image_path = tf.keras.utils.get_file('image'+image_extension, 
-                                         origin=image_url)
-    result, attention_plot = attentionModel.evaluate(image_path)
-    print ('Prediction Caption:', ' '.join(result))
+    result, attention_plot = attentionModel.evaluate2(image_path)
 
-    return JSONResponse({'result':'hi'})
+    return JSONResponse({'result':' '.join(result)})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=5042)
